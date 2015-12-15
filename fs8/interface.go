87878280a58e81ea6183c69d671a237b4afe33bb
He Liu <liulonnie@gmail.com>
2015-12-15 14:30:22 -0800

    .

--- fs8/interface.go
+++ fs8/interface.go
@@ -5,9 +5,16 @@ type FileSys interface {
 	Login(user string) Session
 }
 
+// Commit is a file system commit.
+type Commit struct {
+	ID      int
+	Message string
+}
+
 // Session is a file system login session.
 type Session interface {
 	CheckOut(base int) (int, error)
+	Commits(n int) ([]*Commit, error)
 	ListFiles(c int) ([]string, error)
 	Read(c int, name string) (*string, error)
 	Write(c int, name string, s *string) error
