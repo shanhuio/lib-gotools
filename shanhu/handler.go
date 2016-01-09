0617ae84395135a4f8772822a23ee1935dc48d11
Lonnie Liu <liulonnie@gmail.com>
2016-01-09 08:44:45 -0800

    .

--- shanhu/handler.go
+++ shanhu/handler.go
@@ -2,8 +2,10 @@ package shanhu
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 // Handler is the http handler for shanhu.io website
@@ -23,25 +25,32 @@ func NewHandler(c *Config) (*Handler, error) {
 	}, nil
 }
 
-func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
+func serveFile(w http.ResponseWriter, file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-	path := req.URL.Path
-	log.Println("serving: ", path)
-	c := &context{w: w, req: req}
+	defer f.Close()
+	if _, err := io.Copy(w, f); err != nil {
+		log.Println(err)
+	}
+}
 
+func (h *Handler) serve(c *context, path string) {
 	switch path {
 	case "/github/signin":
 		c.redirect(h.gh.signInURL())
 	case "/github/callback":
-		user, err := h.gh.callback(req)
+		user, err := h.gh.callback(c.req)
 		if err != nil {
 			// print a better error message page
-			fmt.Fprintln(w, err)
+			fmt.Fprintln(c.w, err)
 			return
 		}
 		if user != "h8liu" {
-			fmt.Fprintln(w, err)
+			fmt.Fprintln(c.w, err)
 			return
 		}
 		if user == "h8liu" {
@@ -49,16 +58,37 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			c.writeCookie("session", session, expires)
 			c.redirect("/")
 		}
+	case "/favicon.ico":
+		serveFile(c.w, "_/assets/favicon.ico")
+	case "/style.css":
+		c.w.Header().Set("Content-Type", "text/css")
+		serveFile(c.w, "_/style.css")
+	case "/assets/proj.png":
+		serveFile(c.w, "_/assets/proj.png")
+	case "/assets/pkg.png":
+		serveFile(c.w, "_/assets/pkg.png")
 	default:
 		sessionStr := c.readCookie("session")
 		ok, session := h.sessions.Check(sessionStr)
 
 		if !ok {
 			c.clearCookie("session")
-			fmt.Fprintln(w, `<a href="/github/signin">please sign in</a>`)
+			serveFile(c.w, "_/signin.html")
 			return
 		}
 
-		fmt.Fprintf(w, "You are %s.", session.User)
+		h.serveUser(c, session.User, path)
 	}
+}
+
+func (h *Handler) serveUser(c *context, user, path string) {
+	serveFile(c.w, "_/proj.html")
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+	c := &context{w: w, req: req}
+	path := req.URL.Path
+	log.Println("serving: ", path)
+	h.serve(c, path)
 }
