bbd8618163d2ab29bf1b280ea74376ac0108904b
Lonnie Liu <liulonnie@gmail.com>
2016-01-09 09:56:48 -0800

    .

--- shanhu/handler.go
+++ shanhu/handler.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 // Handler is the http handler for shanhu.io website
@@ -27,6 +28,11 @@ func NewHandler(c *Config) (*Handler, error) {
 
 func serveFile(w http.ResponseWriter, file string) {
 	f, err := os.Open(file)
+	if os.IsNotExist(err) {
+		http.Error(w, "File not found.", 404)
+		return
+	}
+
 	if err != nil {
 		log.Println(err)
 		return
@@ -39,7 +45,18 @@ func serveFile(w http.ResponseWriter, file string) {
 }
 
 func (h *Handler) serve(c *context, path string) {
+	if strings.HasPrefix(path, "/assets/") {
+		serveFile(c.w, "_"+path)
+		return
+	}
+
 	switch path {
+	case "/favicon.ico":
+		serveFile(c.w, "_/assets/favicon.ico")
+	case "/style.css":
+		c.w.Header().Set("Content-Type", "text/css")
+		serveFile(c.w, "_/style.css")
+
 	case "/github/signin":
 		c.redirect(h.gh.signInURL())
 	case "/github/callback":
@@ -58,19 +75,13 @@ func (h *Handler) serve(c *context, path string) {
 			c.writeCookie("session", session, expires)
 			c.redirect("/")
 		}
-	case "/favicon.ico":
-		serveFile(c.w, "_/assets/favicon.ico")
-	case "/style.css":
-		c.w.Header().Set("Content-Type", "text/css")
-		serveFile(c.w, "_/style.css")
-	case "/assets/proj.png":
-		serveFile(c.w, "_/assets/proj.png")
-	case "/assets/pkg.png":
-		serveFile(c.w, "_/assets/pkg.png")
+	case "/github/signout":
+		c.clearCookie("session")
+		c.redirect("/")
+
 	default:
 		sessionStr := c.readCookie("session")
 		ok, session := h.sessions.Check(sessionStr)
-
 		if !ok {
 			c.clearCookie("session")
 			serveFile(c.w, "_/signin.html")
@@ -82,7 +93,23 @@ func (h *Handler) serve(c *context, path string) {
 }
 
 func (h *Handler) serveUser(c *context, user, path string) {
-	serveFile(c.w, "_/proj.html")
+	if strings.HasPrefix(path, "/data/") {
+		h.serveData(c, user, path)
+		return
+	}
+
+	switch path {
+	case "/proj.html", "/":
+		serveFile(c.w, "_/proj.html")
+	case "/file.html":
+		serveFile(c.w, "_/file.html")
+	default:
+		http.Error(c.w, "File not found.", 404)
+	}
+}
+
+func (h *Handler) serveData(c *context, user, path string) {
+	http.Error(c.w, "Data not implemented yet.", 404)
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
