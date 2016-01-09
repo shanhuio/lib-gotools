421daef78bdd7dff2c934f056a58b63013f9a269
Lonnie Liu <liulonnie@gmail.com>
2016-01-08 21:18:50 -0800

    .

--- shanhu/states.go
+++ shanhu/states.go
@@ -1,33 +1,27 @@
 package shanhu
 
 import (
-	"crypto/hmac"
-	"crypto/rand"
-	"crypto/sha256"
 	"encoding/binary"
-	"encoding/hex"
 	"time"
 )
 
+// states serves as a signed timestamp service for checking
+// oauth2 states.
 type states struct {
-	key     []byte
+	s   *signer
+	ttl time.Duration
+
+	// timeNow grants ability to overwrite the time reading
+	// from time package
 	timeNow func() time.Time
-	ttl     time.Duration
 }
 
 func newStates(key []byte, ttl time.Duration) *states {
-	if key == nil {
-		key = make([]byte, 32)
-		_, err := rand.Read(key)
-		if err != nil {
-			panic(err)
-		}
+	return &states{
+		s:   newSigner(key),
+		ttl: ttl,
 	}
-
-	return &states{key: key, ttl: ttl}
 }
-
-const timestampLen = 8
 
 func (s *states) now() time.Time {
 	if s.timeNow == nil {
@@ -36,37 +30,25 @@ func (s *states) now() time.Time {
 	return s.timeNow()
 }
 
-func (s *states) hash(ts []byte) []byte {
-	m := hmac.New(sha256.New, s.key)
-	m.Write(ts)
-	return m.Sum(nil)
-}
+const tsLen = 8
 
 func (s *states) New() string {
-	buf := make([]byte, timestampLen+sha256.Size)
-	ts := buf[:timestampLen]
+	ts := make([]byte, tsLen)
 	now := s.now().UnixNano()
 	binary.LittleEndian.PutUint64(ts, uint64(now))
-	h := s.hash(ts)
-	copy(buf[timestampLen:], h) // append the hash to the end
-	return hex.EncodeToString(buf)
+	return s.s.signHex(ts)
 }
 
 func (s *states) Check(state string) bool {
-	bs, err := hex.DecodeString(state)
-	if err != nil {
-		return false
-	}
-	if len(bs) < timestampLen {
+	ok, ts := s.s.checkHex(state)
+	if !ok {
 		return false
 	}
 
-	ts := bs[:timestampLen]
-	got := bs[timestampLen:]
-	want := s.hash(ts)
-	if !hmac.Equal(got, want) {
+	if len(ts) != tsLen {
 		return false
 	}
+
 	t := int64(binary.LittleEndian.Uint64(ts))
 	if t < 0 {
 		return false
