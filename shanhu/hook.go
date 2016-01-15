7fcfcd7d66e0b225cc4e8c7670c9a5997ad99475
He Liu <liulonnie@gmail.com>
2016-01-14 21:29:40 -0500

    .

--- shanhu/hook.go
+++ shanhu/hook.go
@@ -1,6 +1,8 @@
 package shanhu
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -39,9 +41,43 @@ func (h *hook) serve() {
 }
 
 func (h *hook) update(p string) {
-	log.Printf("updating repo %s", p)
+	log.Printf("updating repo %s (fake)", p)
+}
+
+func mapRepoName(fullName string) string {
+	switch fullName {
+	case "e8vm/e8vm":
+		return "e8vm.io/e8vm"
+	case "e8vm/tools":
+		return "e8vm.io/tools"
+	}
+	return ""
 }
 
 func (h *hook) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	h.req <- "x"
+	payload, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println("read error in hook", err)
+		return
+	}
+
+	var dat struct {
+		Repo struct {
+			FullName string `json:"full_name"`
+		} `json:"repository"`
+	}
+
+	err = json.Unmarshal(payload, &dat)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	path := mapRepoName(dat.Repo.FullName)
+	if path == "" {
+		log.Printf("unrecognized repo: %s", dat.Repo.FullName)
+		return
+	}
+
+	h.req <- path
 }
