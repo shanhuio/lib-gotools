645ebcdb2648bbc903c73327aaacd1e9c3cebe63
He Liu <liulonnie@gmail.com>
2017-01-15 11:52:26 -0600

    line comment check

--- cmd/e8chk/main.go
+++ cmd/e8chk/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go/scanner"
+	"go/token"
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"shanhu.io/smlvm/dagvis"
 	"shanhu.io/smlvm/lexing"
@@ -12,11 +16,21 @@ import (
 	"shanhu.io/tools/goload"
 )
 
-func errExit(e error) {
-	if e == nil {
+func errExit(err error) {
+	if err == nil {
 		return
 	}
-	fmt.Fprintln(os.Stderr, e)
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(-1)
+}
+
+func errsExist(errs []*lexing.Error) {
+	if len(errs) == 0 {
+		return
+	}
+	for _, err := range errs {
+		fmt.Fprintln(os.Stderr, err)
+	}
 	os.Exit(-1)
 }
 
@@ -29,8 +43,8 @@ func checkRectLoaded(prog *goload.Program, h, w int) []*lexing.Error {
 		for _, astf := range pinfo.Files {
 			tokFile := fset.File(astf.Pos())
 			name := tokFile.Name()
-			fin, e := os.Open(name)
-			if lexing.LogError(errs, e) {
+			fin, err := os.Open(name)
+			if lexing.LogError(errs, err) {
 				continue
 			}
 
@@ -41,6 +55,66 @@ func checkRectLoaded(prog *goload.Program, h, w int) []*lexing.Error {
 		}
 	}
 
+	return errs.Errs()
+}
+
+func validLineCommentContent(s string) bool {
+	if strings.HasPrefix(s, " ") {
+		return true
+	}
+	if strings.HasPrefix(s, "\t") {
+		return true
+	}
+	return false
+}
+
+func toLexingPos(p token.Position) *lexing.Pos {
+	return &lexing.Pos{
+		File: p.Filename,
+		Line: p.Line,
+		Col:  p.Column,
+	}
+}
+
+func tokenPos(fset *token.FileSet, pos token.Pos) *lexing.Pos {
+	return toLexingPos(fset.Position(pos))
+}
+
+func checkLineComment(prog *goload.Program) []*lexing.Error {
+	errs := lexing.NewErrorList()
+
+	fset := prog.Fset
+	errHandler := func(pos token.Position, msg string) {
+		errs.Errorf(toLexingPos(pos), "%s", msg)
+	}
+	for _, p := range prog.Pkgs {
+		pinfo := prog.Imported[p]
+		for _, astf := range pinfo.Files {
+			tokFile := fset.File(astf.Pos())
+			s := new(scanner.Scanner)
+
+			bs, err := ioutil.ReadFile(tokFile.Name())
+			if lexing.LogError(errs, err) {
+				continue
+			}
+
+			s.Init(tokFile, bs, errHandler, scanner.ScanComments)
+			for {
+				pos, tok, lit := s.Scan()
+				if tok == token.EOF {
+					break
+				}
+				if tok == token.COMMENT && strings.HasPrefix(lit, "//") {
+					if !validLineCommentContent(lit[2:]) {
+						errs.Errorf(
+							tokenPos(fset, pos),
+							"please add a space to comment %q", lit,
+						)
+					}
+				}
+			}
+		}
+	}
 	return errs.Errs()
 }
 
@@ -66,11 +140,8 @@ func main() {
 	}
 
 	errs := checkRectLoaded(prog, *textHeight, *textWidth)
-	if errs != nil {
-		for _, err := range errs {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		os.Exit(-1)
-	}
+	errsExist(errs)
 
+	errs = checkLineComment(prog)
+	errsExist(errs)
 }
